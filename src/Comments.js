00f1364cbfe5bcd24b496f65f4a7cb53e913d5c8
Nkosi Phillip <internap627@gmail.com>
2019-07-06 07:48:40 +0100

    firebase real time sub

--- src/Comments.js
+++ src/Comments.js
@@ -1,57 +1,64 @@
-import React, {useEffect, useState} from 'react'
-import db from './config/fbConfig'
-import './Comments.css'
+import React, { useEffect, useState } from 'react';
+import db from './config/fbConfig';
+import './Comments.css';
 
-export default function Comments() {
-    const SpeechRecognition = window.SpeechRecognition ||                     window.webkitSpeechRecognition
-    const recognition = new SpeechRecognition()
-    
-    const [comments, setComments] = useState([])
+export default function Comments () {
+  const SpeechRecognition =
+    window.SpeechRecognition || window.webkitSpeechRecognition;
+  const recognition = new SpeechRecognition();
 
-    useEffect(() => {
-        fetchComments()
-    }, [])
+  const [comments, setComments] = useState([]);
 
-    const fetchComments = () => {
-        db.collection('comments').get().then(snapshot => {
-        const dbComments =    snapshot.docs.map(item => item.data())
-        setComments([...dbComments])
-        })
-    }
-    
-    const postComment = (comment) => {
-        let doc = {
-            title: comment
-        }
-        db.collection('comments').add(doc).then(doc => {
-            window.M.toast({html: `${comment}`})
-        })
-    }
+  useEffect(() => {
+    fetchComments();
+  }, []);
 
-    const handleClick = () => {
-        recognition.start()
-        recognition.onresult = (e) => {
-            const current = e.resultIndex
-            const transcript = e.results[current][0].transcript
-            const upperCase = transcript.charAt(0).toUpperCase() + transcript.substring(1);
-            postComment(upperCase)
-            fetchComments()
-        }
-        
-    
-    }
-    console.log(comments)
+  const fetchComments = () => {
+    let dbComments = [];
+    db.collection('comments').onSnapshot(snapshot => {
+      let changes = snapshot.docChanges();
+      changes.forEach(doc => {
+        dbComments = [doc.doc.data(), ...dbComments];
+      });
 
-    return (
-        <div className='comments'>
-            <h1>Comment Section</h1>
-            <button onClick={handleClick }>Click to speak</button>
-            <div className='comment-block'>
-                {comments? 
-                comments.map((comment) => <div key={comment.title} className='blockquote'> <p>{comment.title}</p> </div> 
-                    
-                ) : null}
+      setComments([...dbComments]);
+    });
+  };
+
+  const postComment = comment => {
+    let doc = {
+      title: comment
+    };
+    db.collection('comments').add(doc).then(doc => {
+      window.M.toast({ html: `${comment}` });
+    });
+  };
+
+  const handleClick = () => {
+    recognition.start();
+    recognition.onresult = e => {
+      const current = e.resultIndex;
+      const transcript = e.results[current][0].transcript;
+      const upperCase =
+        transcript.charAt(0).toUpperCase() + transcript.substring(1);
+      postComment(upperCase);
+      //   fetchComments();
+    };
+  };
+
+  return (
+    <div className='comments'>
+      <h1>Comment Section</h1>
+      <button onClick={handleClick}>Click to speak</button>
+      <div className='comment-block'>
+        {comments
+          ? comments.map(comment =>
+            <div key={comment.title} className='blockquote'>
+              {' '}<p>{comment.title}</p>{' '}
             </div>
-        </div>
-    )
+            )
+          : null}
+      </div>
+    </div>
+  );
 }
